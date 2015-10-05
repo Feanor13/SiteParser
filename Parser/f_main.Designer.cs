348cba0fae7eacac9acd94f323131f95966f19ce
Feanor13 <dv6996@gmail.com>
2015-10-05 09:01:01 +0300

    Add parser solution

---
+++ Parser/f_main.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Parser
+{
+    partial class f_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txt_output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(464, 248);
+            this.textBox1.TabIndex = 1;
+            // 
+            // txt_output
+            // 
+            this.txt_output.Location = new System.Drawing.Point(95, 254);
+            this.txt_output.Multiline = true;
+            this.txt_output.Name = "txt_output";
+            this.txt_output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_output.Size = new System.Drawing.Size(464, 212);
+            this.txt_output.TabIndex = 2;
+            // 
+            // f_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 478);
+            this.Controls.Add(this.txt_output);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "f_main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txt_output;
+    }
+}
+
---
+++ Parser/f_main.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Web;
+using System.IO;
+using System.Threading;
+using HtmlAgilityPack;
+
+
+namespace Parser
+{
+    public partial class f_main : Form
+    {
+        //Домен
+        private const string main_url = "http://www.003.ru/krupnaja_i_vstraivaemaja_bytovaja_tehnika-1/gazovye_i_elektricheskie_plity-3/gazovye_plity/_gefest";
+        // Потокобезопасная запись данных в TXT_Output
+        public delegate void add_text(string str);
+        public add_text my_delegate;
+        public void add_text_method(string str)
+        {
+            txt_output.Text += str;
+        }
+        public delegate void set_text(string[] ar);
+        public set_text a_delegate;
+        public void set_text_method(string[] ar)
+        {
+            txt_output.Lines = ar;
+        }
+
+
+        public f_main()
+        {
+            InitializeComponent();
+        }
+        public string getRequest(string url)
+        {
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.AllowAutoRedirect = false;//Запрещаем автоматический реддирект 
+                httpWebRequest.Method = "GET"; //Можно не указывать, по умолчанию используется GET. 
+                httpWebRequest.Referer = "http://rkn.gov.ru/"; // Реферер. Тут можно указать любой URL
+                using (var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+                    using (var stream = httpWebResponse.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(stream, Encoding.GetEncoding(httpWebResponse.CharacterSet)))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                return String.Empty;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string content = getRequest(main_url);
+            string urll; 
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            for (int i=2;i<13;i++)
+            {
+
+                urll = main_url + "/";
+                urll    += i.ToString();
+                content += getRequest(urll);
+            }
+
+                textBox1.Text = content;
+
+            /* HtmlNodeCollection c = doc.DocumentNode.SelectNodes(".//*[@id='priceForm']/div/div/div");
+            foreach (HtmlNode n in c)
+            {
+               
+                textBox1.Text = n.ToString();
+            
+                
+            }*/
+        }
+    }
+}
